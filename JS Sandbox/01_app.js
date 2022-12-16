bcbd67a53b899a2ed42c8ce0993ea6f087eada36
eryarsoyyunusemre <eryarsoyyunusemre@gmail.com>
2022-12-16 12:15:09 +0300

    JS Sandbox

---
+++ JS Sandbox/01_app.js
@@ -0,0 +1,37 @@
+//? -- 1.BOLUM--
+//Yukarıdan mesaj olarak gelir
+alert('Hello World'); 
+
+// Log to console
+
+console.log('Hello World')
+console.log(123)
+console.log(true)
+var greeting = 'Hello'
+console.log(greeting)
+// Array
+console.log([1,2,3,4])
+// Object
+console.log({a:1,b:2})
+// tablo halinde yapar
+console.table({a:1,b:2})
+
+
+// Error verir
+console.error('This is some error')
+// Consolu temizler
+console.clear()
+// Warning verir
+console.warn('This is some warning')
+// timer başlangıç ile bitiş arasındaki süreyi verir
+ 
+console.time('Hello')
+console.log('Hello World')
+console.log(123)
+console.log(true)
+var greeting = 'Hello'
+console.log(greeting)
+console.timeEnd('Hello')
+
+console.clear()
+
---
+++ JS Sandbox/03_app.js
@@ -0,0 +1,38 @@
+//! -- DATA TYPES IN JAVASCRİPT --
+
+// PRIMITIVE
+
+// String
+const name ='Yunus'
+// Number
+const age = 20
+// Boolean
+const hasKids= false;
+// Null
+const car = null;
+// Undefined
+let text;
+// Symbol
+const sym =Symbol()
+
+console.log(typeof name,name)
+console.log(typeof age,age)
+console.log(typeof hasKids,hasKids)
+console.log(typeof car,car)
+console.log(typeof text,text)
+console.log(typeof sym,sym)
+
+// REFERENCES TYPES - Objects
+// Array
+const hobbies =['movies','music']
+// Object literal
+const adress = {
+    city:'Kocaeli',
+    state:'Korfez'
+}
+const today = new Date()
+
+
+console.log(typeof hobbies,hobbies)
+console.log(typeof adress,adress)
+console.log(typeof today,today)
