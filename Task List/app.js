63dfc3cf6a40b7fcbd01b24d2cf56c38be9fa711
Yunus Emre Eryarsoy <eryaroyyunusemre@gmail.com>
2022-12-21 12:04:04 +0300

    Task List

--- Task List/app.js
+++ Task List/app.js
@@ -12,6 +12,12 @@ loadEventListeners();
 function loadEventListeners() {
   // Add task event
   form.addEventListener('submit', addTask);
+  // Remove task event
+  taskList.addEventListener('click',removeTask)
+  // Clear task event 
+  clearBtn.addEventListener('click',clearTask)
+  // Filter task event
+  filter.addEventListener('keyup',filterTask)
 }
 
 // Add Task
@@ -43,3 +49,40 @@ function addTask(e) {
 
   e.preventDefault();
 }
+
+// Remove task
+function removeTask(e) {
+  if(e.target.parentElement.classList.contains('delete-item')){
+    console.log('delete item');
+    if(confirm('Are you sure')){
+    e.target.parentElement.parentElement.remove()
+    }
+  }
+}
+
+// Clear Task
+function clearTask(e) {
+
+  // taskList.innerHTML=''
+
+  // Faster == Remove child
+  while(taskList.firstChild) {
+    taskList.removeChild(taskList.firstChild)
+  }
+}
+
+// Filter Tasks
+function filterTask(e) {
+  const text = e.target.value.toLowerCase();
+  
+  document.querySelectorAll('.collection-item').forEach(
+    function(task){
+      const item = task.firstChild.textContent
+      if(item.toLowerCase().indexOf(text) != -1){
+        task.style.display = 'block'
+      }else{
+        task.style.display = 'none'
+      }
+    }
+  )
+} 
