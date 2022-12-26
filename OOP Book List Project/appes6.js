b611f160e9df747df595bdc1fa33e6319a065375
Yunus Emre Eryarsoy <eryaroyyunusemre@gmail.com>
2022-12-26 15:09:23 +0300

    Book List Project

--- OOP Book List Project/appes6.js
+++ OOP Book List Project/appes6.js
@@ -52,8 +52,53 @@ class UI {
         document.getElementById('author').value = ''
         document.getElementById('isbn').value = ''
     }
+
+}
+// Local Storage Class
+class Store {
+    static getBooks(){
+        let books
+        if (localStorage.getItem('books') === null){
+             books =[]
+        }else{
+             books = JSON.parse(localStorage.getItem('books'))
+        }
+
+        return books
+    }
+
+    static displayBooks(){
+        const books = Store.getBooks()
+
+        books.forEach(function (book){
+            const ui = new UI
+
+            // Add book to UI
+            ui.addBookToList(book)
+        })
+    }
+
+    static addBook(book){
+        const books = Store.getBooks()
+
+        books.push(book)
+
+        localStorage.setItem('books',JSON.stringify(books))
+    }
+    static removeBook(isbn){
+        const books = Store.getBooks()
+
+        books.forEach(function (book,index){
+            if (book.isbn === isbn){
+                books.splice(index,1)
+            }
+        })
+        localStorage.setItem('books',JSON.stringify(books))
+    }
 }
 
+// DOM Load Event
+document.addEventListener('DOMContentLoaded',Store.displayBooks)
 
 // Event Listener for add
 document.getElementById('book-form').addEventListener("submit", function (e){
@@ -77,6 +122,9 @@ document.getElementById('book-form').addEventListener("submit", function (e){
         // Add Book to List
         ui.addBookToList(book);
 
+        // Add to LS
+        Store.addBook(book)
+
         // Show success
         ui.showAlert('Book Added!','success')
 
@@ -91,6 +139,8 @@ document.getElementById('book-form').addEventListener("submit", function (e){
 document.getElementById('book-list').addEventListener('click',function (e){
     const ui = new UI()
     ui.deleteBook(e.target)
+    // Remove from LS
+    Store.removeBook(e.target.parentElement.previousElementSibling.textContent)
     ui.showAlert('Book Removed!','success')
     e.preventDefault()
 })
