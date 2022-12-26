48d7f6fd9efec992945b8c35e5f07117059721af
Yunus Emre Eryarsoy <eryaroyyunusemre@gmail.com>
2022-12-26 14:12:37 +0300

    Book List Project

--- OOP Book List Project/app.js
+++ OOP Book List Project/app.js
@@ -26,8 +26,28 @@ UI.prototype.addBookToList = function (book){
     list.appendChild(row)
 }
 
-// Clear Fields
+// Show Alert
+UI.prototype.showAlert = function (message,className){
+    // Create div
+    const div = document.createElement("div")
+    // Add classes
+    div.className = `alert ${className}`
+    // Add text
+    div.appendChild(document.createTextNode(message))
+    // Get parent
+    const container = document.querySelector('.container')
+    // Get Form
+    const form = document.querySelector('#book-form')
+    // Insert Alert
+    container.insertBefore(div,form)
 
+    // Timeout
+    setTimeout(function (){
+        document.querySelector('.alert').remove()
+    },3000)
+}
+
+// Clear Fields
 UI.prototype.clearFields = function (){
     document.getElementById('title').value = ''
     document.getElementById('author').value = ''
@@ -48,11 +68,23 @@ document.getElementById('book-form').addEventListener("submit", function (e){
     // Instantiate UI
     const ui = new UI()
 
-    // Add Book to List
-    ui.addBookToList(book);
+    // Validate
+    if (title ===''|| author ===''|| isbn ===''){
+      // Error alert
+        ui.showAlert('Please fill in all fields','error')
+    }else{
+        // Add Book to List
+        ui.addBookToList(book);
 
-    // Clear field
-    ui.clearFields()
+        // Show success
+        ui.showAlert('Book Added!','success')
+
+        // Clear field
+        ui.clearFields()
+    }
+
+
+
 
 
 
