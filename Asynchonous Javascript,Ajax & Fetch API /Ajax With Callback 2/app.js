eac11bdf3a6b233ba81a61899665f6416cda5f98
Yunus Emre Eryarsoy <eryaroyyunusemre@gmail.com>
2023-02-22 18:21:15 +0300

    new

--- Asynchonous Javascript,Ajax & Fetch API /Ajax With Callback 2/app.js
+++ Asynchonous Javascript,Ajax & Fetch API /Ajax With Callback 2/app.js
@@ -39,13 +39,22 @@ const data = {
 
 
 //Update Post
-http.put('https://jsonplaceholder.typicode.com/posts/1',
-data,function(err,post){
-    if(err){
+// http.put('https://jsonplaceholder.typicode.com/posts/1',
+// data,function(err,post){
+//     if(err){
+//     console.log(err)
+//   }else{
+//     console.log(post) 
+//   }
+// })
+
+
+// Delete Post
+http.delete('https://jsonplaceholder.typicode.com/posts/1',
+function(err,response){
+  if(err){
     console.log(err)
   }else{
-    console.log(post) 
+    console.log(response) 
   }
 })
-
- 
