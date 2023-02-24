8241449d3ef1094375462b732e7c130004d6bd43
Yunus Emre Eryarsoy <eryaroyyunusemre@gmail.com>
2023-02-24 15:01:00 +0300

    Github Finder

--- API Projects/Github Finder/github.js
+++ API Projects/Github Finder/github.js
@@ -2,13 +2,17 @@ class Github {
   constructor() {
     this.client_id = 'd9308aacf8b204d361fd';
     this.client_secret = '84969aeef73956f4ec9e8716d1840532802bb81b';
+    this.repos_count = 5;
+    this.repos_sort = 'created: asc'
   }
 
   async getUser(user) {
     const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
+    const repoResponse = await fetch(`https://api.github.com/users/${user}/repos?client_id=${this.client_id}&client_secret=${this.client_secret}`);
 
     const profile = await profileResponse.json();
 
+    
     return {
       profile
     }
