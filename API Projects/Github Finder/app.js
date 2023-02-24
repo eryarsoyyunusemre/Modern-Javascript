07a9c1e424cac9468fd98f9eb964eb05577f7002
eryarsoyyunusemre <eryarsoyyunusemre@gmail.com>
2023-02-24 14:37:57 +0300

    Github Finder

--- API Projects/Github Finder/app.js
+++ API Projects/Github Finder/app.js
@@ -1,6 +1,7 @@
 // Init Github
-const github = new Github()
-
+const github = new Github;
+// Init UI
+const ui = new UI;
 
 // Search input
 const searchUser = document.getElementById('searchUser')
@@ -18,6 +19,7 @@ searchUser.addEventListener('keyup',(e)=>{
       // Show alert
     }else{
       // Show profile
+      ui.showProfile(data.profile)
     }
   
   })
