1cadada861769ce29cbb0f9ba09e0540890c8202
eryarsoyyunusemre <eryarsoyyunusemre@gmail.com>
2023-02-24 15:01:00 +0300

    Github Finder

--- API Projects/Github Finder/app.js
+++ API Projects/Github Finder/app.js
@@ -4,27 +4,27 @@ const github = new Github;
 const ui = new UI;
 
 // Search input
-const searchUser = document.getElementById('searchUser')
+const searchUser = document.getElementById('searchUser');
 
 // Search input event listener
-searchUser.addEventListener('keyup',(e)=>{
+searchUser.addEventListener('keyup', (e) => {
   // Get input text
   const userText = e.target.value;
 
-  if(userText !==''){
+  if(userText !== ''){
    // Make http call
    github.getUser(userText)
-   .then(data => {
-    if(data.profile.message === 'Not Found'){
-      // Show alert
-    }else{
-      // Show profile
-      ui.showProfile(data.profile)
-    }
-  
-  })
-  }
-  else{
+    .then(data => {
+      if(data.profile.message === 'Not Found') {
+        // Show alert
+        ui.showAlert('User not found', 'alert alert-danger');
+      } else {
+        // Show profile
+        ui.showProfile(data.profile);
+      }
+    })
+  } else {
     // Clear profile
+    ui.clearProfile();
   }
-})
+}); 
