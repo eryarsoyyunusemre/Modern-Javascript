c30f78ffe218962c65bca5f7854f539df63a1a4d
eryarsoyyunusemre <eryarsoyyunusemre@gmail.com>
2023-02-25 15:26:34 +0300

    Weather JS

--- API Projects/WeatherJS/app.js
+++ API Projects/WeatherJS/app.js
@@ -1,8 +1,36 @@
-// Init Weather object
-const weather = new Weather('Boston','MA');
+const url = 'https://api.openweathermap.org/data/2.5/' ;
+const key  = '15f13747816da06d0767cd3cdd813cfe';
 
-weather.getWeather()
-.then(results => {
-  console.log(results);
-})
-.catch(err => console.log(err));
+const searchBar = document.querySelector('#searchBar')
+searchBar.addEventListener('keypress',setQuery)
+
+function setQuery(e){
+  if(e.keyCode =='13'){
+    getResult(searchBar.value)
+  }
+}
+
+function getResult(cityName){
+ let query = `${url}weather?q=${cityName}&appid=${key}&units=metric&lang=tr`
+  fetch(query)
+  .then(weather =>{
+    return weather.json()
+  })
+  .then(displayResult)
+}
+
+function displayResult(result){
+ let city = document.querySelector('.city')
+ city.innerText = `${result.name},${result.sys.country}`
+
+ let temp = document.querySelector('.temp')
+ temp.innerText = `${Math.round(result.main.temp)}°C`
+ 
+ let desc = document.querySelector('.desc')
+ desc.innerText = result.weather[0].description;
+
+ let minmax = document.querySelector('.minmax')
+ minmax.innerText = `${Math.round(result.main.temp_min)}°C / ${Math.round(result.main.temp_max)}°C `
+}
+
+searchBar.addEventListener('keypress',setQuery)
