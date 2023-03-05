7143826663c4342bf68dd15c305f267fbe2b223f
Yunus Emre Eryarsoy <eryaroyyunusemre@gmail.com>
2023-03-05 12:23:12 +0300

    Tracalorie Project

--- Javascript Patterns/Tracalorie Project /app.js
+++ Javascript Patterns/Tracalorie Project /app.js
@@ -12,9 +12,9 @@ const ItemCtrl = (function(){
  // Data Structure / State
  const data = {
   items:[
-    {id:0,name:'Steak Dinner',calories:1200},
-    {id:1,name:'Cookie',calories:400}, 
-    {id:2,name:'Eggs',calories:300}
+    // {id:0,name:'Steak Dinner',calories:1200},
+    // {id:1,name:'Cookie',calories:400}, 
+    // {id:2,name:'Eggs',calories:300}
   ],
   currentItem:null,
   totalCalories:0
@@ -42,7 +42,7 @@ const ItemCtrl = (function(){
 
      // Add to items array
      data.items.push(newItem)
-     
+
      return newItem;
   },
   logData: function(){
@@ -85,6 +85,32 @@ const UICtrl = (function(){
         calories : document.querySelector(UISelectors.itemCalories).value
       }
     },
+    addListItem:function(item){
+      //Show the list
+      document.querySelector(UISelectors.itemLists).style.display = 'block';
+      // Create li element
+      const li = document.createElement('li');
+      //Add class
+      li.className = 'collection-item';
+      // Add ID
+      li.id = `item-${item.id}`
+      //Add HTML
+      li.innerHTML = ` 
+      <strong>${item.name}: </strong> <em>${item.calories} Calories</em>
+      <a href="#" class="secondary-content">
+        <i class="edit-item fa fa-pencil"></i>
+      </a>`;
+      // Insert item
+      document.querySelector(UISelectors.itemLists).
+      insertAdjacentElement('beforeend',li)
+    },
+    clearInput:function(){
+      document.querySelector(UISelectors.itemName).value ='';
+      document.querySelector(UISelectors.itemCalories).value ='';
+    },
+    hideList:function(){
+      document.querySelector(UISelectors.itemLists).style.display = 'none';
+    },
     getSelectors: function(){
       return UISelectors
     }
@@ -113,6 +139,12 @@ const App = (function(ItemCtrl,UICtrl){
     if(input.name!==''&&  input.calories!==''){
       // Add İtem
       const newItem = ItemCtrl.addItem(input.name,input.calories);
+
+      // Add item to UI list
+      UICtrl.addListItem(newItem)
+
+      // Clear fields
+      UICtrl.clearInput()
     }
 
 
@@ -125,9 +157,16 @@ return {
   init: function(){
     // Fetch items from data structure
     const items = ItemCtrl.getItems();
-    
+
+    // Check if any items
+    if(items.length === 0){
+      UICtrl.hideList();
+    }else{  
     // Populate list with items
     UICtrl.populateItemList(items);
+    }
+    
+  
 
     // Load event listeners
     loadEventListeners();
