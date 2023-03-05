37873ed0aed004e9ddfff5daf64a859f241f0b65
Yunus Emre Eryarsoy <eryaroyyunusemre@gmail.com>
2023-03-05 15:13:29 +0300

    Tracalorie Project

--- Javascript Patterns/Tracalorie Project /app.js
+++ Javascript Patterns/Tracalorie Project /app.js
@@ -45,6 +45,19 @@ const ItemCtrl = (function(){
 
      return newItem;
   },
+  getTotalCalories: function(){
+    let total = 0;
+    // Loop through items and add cals
+    data.items.forEach(function(item){
+        total+= item.calories;
+    })
+
+    // Set total cal in data structure
+    data.totalCalories = total
+    // Return total
+    return data.totalCalories;
+   
+  },
   logData: function(){
     return data;
   }
@@ -59,6 +72,7 @@ const UICtrl = (function(){
     addBtn:'.add-btn',
     itemName:'#item-name',
     itemCalories:'#item-calories',
+    totalCal:'.total-calories'
   }
  
   // Public Methods
@@ -111,6 +125,9 @@ const UICtrl = (function(){
     hideList:function(){
       document.querySelector(UISelectors.itemLists).style.display = 'none';
     },
+    showTotalCalories:function(totalCalories){
+     document.querySelector(UISelectors.totalCal).textContent = totalCalories
+    },
     getSelectors: function(){
       return UISelectors
     }
@@ -143,6 +160,12 @@ const App = (function(ItemCtrl,UICtrl){
       // Add item to UI list
       UICtrl.addListItem(newItem)
 
+      // Get total calories
+      const totalCalories = ItemCtrl.getTotalCalories();
+
+      // Add total calories to UI
+      UICtrl.showTotalCalories(totalCalories)
+
       // Clear fields
       UICtrl.clearInput()
     }
@@ -166,6 +189,11 @@ return {
     UICtrl.populateItemList(items);
     }
     
+      // Get total calories
+      const totalCalories = ItemCtrl.getTotalCalories();
+
+      // Add total calories to UI
+      UICtrl.showTotalCalories(totalCalories)
   
 
     // Load event listeners
@@ -173,7 +201,7 @@ return {
   }
 }
 
-})(ItemCtrl,UICtrl);
+})(ItemCtrl, UICtrl);
 
 // Initializing App.
 App.init()
